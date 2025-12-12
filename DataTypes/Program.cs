d771de5ad3f9216f9ff7f83c48bd43052ff7b21f
bartodes <bartojdes@gmail.com>
2025-12-12 18:13:54 +0000

    initial commit

---
+++ DataTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿// Podemos definir la variable con su tipo y despues pasarle un valor
+int age;
+age = 23;
+
+Console.WriteLine(age);
+
+// O hacerlo en un misma linea
+long num = 9000000; // Poniendo una L mayuscula al final nos aseguramos que sea de tipo Long sino lo  especificamos al inicializar la var
+Console.WriteLine(num);
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+
+
+double negnum = -23.09D;
+Console.WriteLine(negnum);
