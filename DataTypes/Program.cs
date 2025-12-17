a74e91a6f551efed63967fa5508a4630ec2536e3
bartodes <bartojdes@gmail.com>
2025-12-17 20:14:00 +0000

    added operators

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,4 +1,6 @@
 ﻿// NUMBERS //
+using System.Security.Cryptography;
+
 int age;
 age = 23;
 
@@ -52,3 +54,51 @@ string textNum = "103";
 int someNum = Convert.ToInt32(textNum);
 Console.WriteLine(textNum.GetType());
 Console.WriteLine(someNum.GetType());
+
+string textBigNum = "-90000000000";
+long bigNum = Convert.ToInt64(textBigNum);
+
+Console.WriteLine(bigNum);
+Console.WriteLine(bigNum.GetType());
+
+// BOOLEAN //
+
+bool someVal = true;
+Console.WriteLine(someVal);
+someVal = false;
+Console.WriteLine(someVal);
+
+
+// OPERATORS //
+
+Console.WriteLine(age);
+age++;
+Console.WriteLine(age);
+age--;
+Console.WriteLine(age);
+age += 4;
+Console.WriteLine(age);
+age -= 4;
+Console.WriteLine(age);
+age *= 3;
+Console.WriteLine(age);
+age /= 2; // When we divide two int, we obtain a rounded resoult
+Console.WriteLine(age);
+double someDouble = 69;
+someDouble /= 2;
+Console.WriteLine(someDouble);
+
+// Concatenation with + operator
+string student = "Pepe";
+student += " esta programando en C#!";
+Console.WriteLine(student);
+
+// With char type it sums the unicode and gives us the char it is equivalent to
+char c = 'a';
+c += 'b';
+Console.WriteLine(c);
+
+// Operator ++ and -- sequence
+int i = 0;
+Console.WriteLine(i++); // When the ++ is after the var it excecutes after 
+Console.WriteLine(i);
